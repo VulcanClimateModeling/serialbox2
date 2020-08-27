97f2b2ab985e30977206b9a8a4605667caefa6a1
Oliver Fuhrer <oli@ginko.ch>
2020-08-27 09:34:10 -0700

    add comment and change argument order

--- src/serialbox-fortran/utils_ppser.f90
+++ src/serialbox-fortran/utils_ppser.f90
@@ -63,10 +63,11 @@ CONTAINS
 
 !============================================================================
 
-SUBROUTINE ppser_initialize(directory, directory_ref, prefix, mode, prefix_ref, mpi_rank, rprecision, rperturb, realtype, archive, unique_id)
+SUBROUTINE ppser_initialize(directory, prefix, mode, directory_ref, prefix_ref, &
+                mpi_rank, rprecision, rperturb, realtype, archive, unique_id)
   CHARACTER(LEN=*), INTENT(IN)           :: directory, prefix
   INTEGER, OPTIONAL, INTENT(IN)          :: mode
-  CHARACTER(LEN=*), OPTIONAL, INTENT(IN) :: prefix_ref, directory_ref
+  CHARACTER(LEN=*), OPTIONAL, INTENT(IN) :: prefix_ref, directory_ref  ! use a different serializer for reading (optional)
   INTEGER, OPTIONAL, INTENT(IN)          :: mpi_rank
   REAL(KIND=8), OPTIONAL, INTENT(IN)     :: rprecision, rperturb
   INTEGER, OPTIONAL, INTENT(IN)          :: realtype
